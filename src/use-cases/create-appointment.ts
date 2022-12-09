b3d344d1e166b25610d66929958d2a0fe12b38f8
Rodrigo <rodrigorael53@gmail.com>
2022-12-08 21:27:23 -0300

    creating the create-appointment use-case

---
+++ src/use-cases/create-appointment.ts
@@ -0,0 +1,30 @@
+import { Appointment } from '../entities/appointment';
+
+// todo caso de uso tem uma entrada(Request)
+interface CreateAppointmentRequest {
+  // para criar um nodo agendamento(appointment) eu predico dos dados:
+  customer: string;
+  startsAt: Date;
+  endsAt: Date;
+}
+
+// e uma saída(Response)
+// no final, quando eu terminar de criar um agendamento(appointment) eu vou
+// devolver um Appointment
+type CreateAppointmentResponse = Appointment;
+
+export class CrateAppointment {
+  async execute ({
+    customer, 
+    startsAt, 
+    endsAt,
+  }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const appointment = new Appointment({
+      customer,
+      startsAt,
+      endsAt,
+    });
+
+    return appointment;
+  }
+}
